b04b7f91b8b70efe649217dd5c95f8cb4a92546f
linjin1130 <jin.lin@quantum-info.com>
2018-10-10 16:50:42 +0800

    数据源不更新数据ttl时间后，监视列表自动清除

--- client1.py
+++ client1.py
@@ -2,6 +2,7 @@
 import asyncio
 import json
 
+import time
 import websockets
 
 from DAboarddata import DAbroaddata
@@ -14,13 +15,15 @@ da_parse = DAStatusParser('info.json')
 da_dict = {}
 da_datas = []
 pre_color = []
-
+ttl = 5
 import config
 ip = config.ip
 port = config.port
 use_false_data = config.use_false_data
+last_time = time.time()
 async def chat():
     global pre_color
+    global last_time
     async with websockets.connect(
             'ws://'+ip+':'+str(port)+'/ws') as websocket:
         print('监视数据获取客户端打开')
@@ -28,10 +31,26 @@ async def chat():
             # 通过网络监视接口，获取原始监视数据
             message, addr = udp_rc.dabroad_datareq(false_data=use_false_data)
             msg, color = da_parse.bytes_to_str(message)
-            da_dict[msg[3]['value']] = color
+            da_dict[msg[3]['value']] = [color,ttl]
             # da_set.add(msg[3]['value'])
             # da_list = list(da_set)
             # da_dict.sort()
+            if (time.time() - last_time) > 1:
+                rm_key = []
+                for key in da_dict:
+                    print(da_dict[key])
+                    val = da_dict[key]
+                    val[1] -= 1
+                    da_dict[key] = val
+                    if val[1] < 1:
+                        rm_key.append(key)
+                        print(f'数据源{key}不再更新')
+                for key in rm_key:
+                    da_dict.pop(key)
+                last_time = time.time()
+                print(time.time() - last_time)
+
+
             da_lists = sorted(da_dict.items())#[[key, value] for key, value in da_dict]
             da_list = [item[0] for item in da_lists]
             color_list = [item[1] for item in da_lists]
